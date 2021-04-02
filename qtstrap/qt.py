3a0d2c1bd6f5f2e258927742b3f5ccde9a2a0152
DaelonSuzuka <daelonsuzuka@gmail.com>
2021-04-02 15:59:01 -0400

    Switched to using qtpy

--- qtstrap/qt.py
+++ qtstrap/qt.py
@@ -1,12 +1,5 @@
-import PySide2
-from PySide2 import QtCore, QtGui, QtWidgets
-from PySide2.QtCore import *
-from PySide2.QtGui import *
-from PySide2.QtWidgets import *
-
-from PySide2.QtNetwork import *
-from PySide2.QtWebSockets import *
-from PySide2.QtCharts import *
-from PySide2.QtMultimedia import *
-from PySide2.QtSerialPort import *
-from PySide2.QtSql import *
+import qtpy 
+from qtpy import QtCore, QtGui, QtWidgets
+from qtpy.QtCore import *
+from qtpy.QtGui import *
+from qtpy.QtWidgets import *
