52ce0d0034a7374182baff5d517b61f077a985ad
Daelon Suzuka <daelonsuzuka@gmail.com>
2021-12-19 03:14:17 -0500

    Improved TimeStamp api

--- qtstrap/utils/timestamp.py
+++ qtstrap/utils/timestamp.py
@@ -9,8 +9,17 @@ class TimeStamp:
     def __init__(self):
         self._time = time()
 
-    def time_since(self, timestamp):
-        return (time() - timestamp)
+    def time_since(self, timestamp=None):
+        if timestamp is None:
+            return time() - self._time
+            
+        if type(timestamp) == TimeStamp():
+            return time() - timestamp._time
+
+        return time() - timestamp
 
     def update(self):
         self._time = time()
+
+    def __repr__(self):
+        return str(self._time)
