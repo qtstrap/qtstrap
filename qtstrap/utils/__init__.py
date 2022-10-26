2908a969ae009bae78d0a4b3a7dac3e9979811c9
David Kincaid <daelonsuzuka@gmail.com>
2022-10-26 02:49:48 -0400

    Add Builder class

--- qtstrap/utils/__init__.py
+++ qtstrap/utils/__init__.py
@@ -6,5 +6,6 @@ from .drag_and_drop import accepts_file_drops
 from .get_ip import get_ip
 from .singleton import singleton
 from .signals import SignalBlocker
+from .string_builder import Builder
 from .timestamp import time_since, TimeStamp
 from .utils import *
---
+++ qtstrap/utils/string_builder.py
@@ -0,0 +1,33 @@
+
+
+class Builder:
+    """
+    Utility class for incrementally building strings.
+    """
+    def __init__(self, out=None, indent=4):
+        self.items = []
+        if out:
+            self.out = out
+        else:
+            self.out = self.items.append
+        self.indent = indent
+        self.level = 0
+
+    def __lshift__(self, item):
+        self.line(item)
+    
+    def line(self, string=''):
+        if string:
+            self.out(' ' * self.indent * self.level + string + '\n')
+        else:
+            self.out('\n')
+
+    def join(self, base:str):
+        return base.join(self.items)
+
+    def __enter__(self):
+        self.level += 1
+        return self
+
+    def __exit__(self, *_):
+        self.level -= 1
