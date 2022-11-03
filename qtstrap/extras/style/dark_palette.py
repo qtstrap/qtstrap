448d8c3714bbe37a015aedd4da62c1aa8f033117
David Kincaid <daelonsuzuka@gmail.com>
2022-11-03 14:17:35 -0400

    Rearrange darkPalette to prep for future revision

--- qtstrap/extras/style/dark_palette.py
+++ qtstrap/extras/style/dark_palette.py
@@ -11,23 +11,30 @@ accent= QColor('#ca3e47')
 
 
 # base
-darkPalette.setColor(QPalette.WindowText, QColor(180, 180, 180))
-darkPalette.setColor(QPalette.Button, QColor(53, 53, 53))
-darkPalette.setColor(QPalette.Light, QColor(180, 180, 180))
-darkPalette.setColor(QPalette.Midlight, QColor(90, 90, 90))
-darkPalette.setColor(QPalette.Dark, QColor(35, 35, 35))
-darkPalette.setColor(QPalette.Text, QColor(180, 180, 180))
-darkPalette.setColor(QPalette.BrightText, QColor(180, 180, 180))
-darkPalette.setColor(QPalette.ButtonText, QColor(180, 180, 180))
-darkPalette.setColor(QPalette.Base, QColor(42, 42, 42))
 darkPalette.setColor(QPalette.Window, QColor(53, 53, 53))
-darkPalette.setColor(QPalette.Shadow, QColor(20, 20, 20))
-darkPalette.setColor(QPalette.Highlight, QColor(42, 130, 218))
-darkPalette.setColor(QPalette.HighlightedText, QColor(180, 180, 180))
-darkPalette.setColor(QPalette.Link, QColor(56, 252, 196))
+darkPalette.setColor(QPalette.WindowText, QColor(180, 180, 180))
+darkPalette.setColor(QPalette.Base, QColor(42, 42, 42))
 darkPalette.setColor(QPalette.AlternateBase, QColor(66, 66, 66))
 darkPalette.setColor(QPalette.ToolTipBase, QColor(53, 53, 53))
 darkPalette.setColor(QPalette.ToolTipText, QColor(180, 180, 180))
+# darkPalette.setColor(QPalette.PlaceholderText, QColor(180, 180, 180))
+darkPalette.setColor(QPalette.Text, QColor(180, 180, 180))
+
+darkPalette.setColor(QPalette.Button, QColor(53, 53, 53))
+darkPalette.setColor(QPalette.ButtonText, QColor(180, 180, 180))
+darkPalette.setColor(QPalette.BrightText, QColor(180, 180, 180))
+
+darkPalette.setColor(QPalette.Light, QColor(180, 180, 180))
+darkPalette.setColor(QPalette.Midlight, QColor(90, 90, 90))
+darkPalette.setColor(QPalette.Dark, QColor(35, 35, 35))
+# darkPalette.setColor(QPalette.Mid, QColor(35, 35, 35))
+darkPalette.setColor(QPalette.Shadow, QColor(20, 20, 20))
+
+darkPalette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+darkPalette.setColor(QPalette.HighlightedText, QColor(180, 180, 180))
+
+darkPalette.setColor(QPalette.Link, QColor(56, 252, 196))
+# darkPalette.setColor(QPalette.LinkVisited, QColor(56, 252, 196))
 
 # disabled
 darkPalette.setColor(QPalette.Disabled, QPalette.WindowText, QColor(127, 127, 127))
