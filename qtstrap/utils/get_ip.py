512cafc522a1ea9b65df6fe8bb556be276956743
David Kincaid <daelonsuzuka@gmail.com>
2022-11-03 14:20:47 -0400

    Improved get_ip() documentation and add possible future improvement

--- qtstrap/utils/get_ip.py
+++ qtstrap/utils/get_ip.py
@@ -2,9 +2,25 @@ import socket
 
 
 def get_ip():
-    # this is a dirty hack to get our current machine's IP address
+    """
+    Get the current machine's IPv4 address.
+
+    This process is more complicated than it appears because of the possiblity of multiple arbitrary network interfaces.
+    """
+
+    # possible alternate implementation
+    # it's cleaner but partially broken
+    # hostname = socket.gethostname()
+    # print(hostname)
+    # ip = socket.gethostbyname(hostname)
+    # return ip
+
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     ip = s.getsockname()[0]
     s.close()
     return ip
+
+
+if __name__ == "__main__":
+    print(get_ip())
