24af4edc8b896588385eac0bbb4b2803bbe37a1e
David Kincaid <daelonsuzuka@gmail.com>
2022-12-08 14:05:55 -0500

    Add timestamps to trace decorator

--- qtstrap/utils/trace.py
+++ qtstrap/utils/trace.py
@@ -1,9 +1,14 @@
+from time import time
+
+
+start_time = time()
 
 
 def trace(func):
-    print('tracing', func)
+    print(f'[{time() - start_time:.3f}] tracing', func)
+
     def new(*args, **kwargs):
-        print('executing', func, args, kwargs)
+        print(f'[{time() - start_time:.3f}] executing', func, args, kwargs)
         func(*args, **kwargs)
 
     return new
