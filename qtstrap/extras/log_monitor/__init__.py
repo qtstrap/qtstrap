3d7f84e50b738263914c09b1323958da52152093
Daelon Suzuka <daelonsuzuka@gmail.com>
2022-12-21 16:47:51 -0500

    Add rough draft of excepthook

--- qtstrap/extras/log_monitor/__init__.py
+++ qtstrap/extras/log_monitor/__init__.py
@@ -2,12 +2,22 @@ from .log_widget import LogMonitorWidget, LogMonitorDockWidget, LogMonitorDropdo
 from .log_database_handler import DatabaseHandler
 import logging
 from qtstrap import OPTIONS
+import sys
 
 
 db_path = (OPTIONS.config_dir / 'log.db').as_posix()
+exception_logger_name = 'exceptions'
 
 
-def install(database_name=db_path):
+def install(database_name=db_path, install_excepthook=False):
     logger = logging.getLogger()
     logger.setLevel(1)
     logger.addHandler(DatabaseHandler(database_name))
+
+    exception_logger = logging.getLogger(exception_logger_name)
+    def handle_exception(exc_type, exc_value, exc_traceback):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        exception_logger.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+
+    if install_excepthook:
+        sys.excepthook = handle_exception
