627cc882d830103f5e123e490017eeab77c61337
David Kincaid <daelonsuzuka@gmail.com>
2023-03-07 07:09:11 -0500

    Add qt helpers

--- qtstrap/qt.py
+++ qtstrap/qt.py
@@ -3,3 +3,29 @@ from qtpy import QtCore, QtGui, QtWidgets
 from qtpy.QtCore import *
 from qtpy.QtGui import *
 from qtpy.QtWidgets import *
+
+
+"""
+helper that shortens signal connections
+
+object.signal.connect(handler) -> object.signal(handler)
+
+@object.signal
+def handler():
+    pass
+"""
+if qtpy.PYQT_VERSION:
+    pyqtBoundSignal.__call__ = pyqtBoundSignal.connect
+elif qtpy.PYSIDE_VERSION:
+    SignalInstance.__call__ = SignalInstance.connect
+
+"""
+helper that allows for easily finding QObjects by name
+
+some_widget['name_of_child']
+
+@widget['button1'].clicked
+def button1_hander():
+    pass
+"""
+QObject.__getitem__ = lambda self, name: self.findChild(QWidget, name)
