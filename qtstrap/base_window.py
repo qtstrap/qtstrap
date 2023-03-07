7a1fa777463ce68f03eb2cde17f3b73e07e1b7fa
David Kincaid <daelonsuzuka@gmail.com>
2023-03-07 07:39:23 -0500

    Formatting changes

--- qtstrap/base_window.py
+++ qtstrap/base_window.py
@@ -24,24 +24,24 @@ class ThemeMenu(QMenu):
 class BaseMainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-            
-        self.setObjectName("MainWindow")
+
+        self.setObjectName('MainWindow')
         self.load_settings()
 
     def closeEvent(self, event):
         self.save_settings()
         super().closeEvent(event)
-        
+
     def save_settings(self):
-        QSettings().setValue("mainwindow/geometry", self.saveGeometry())
-        QSettings().setValue("mainwindow/state", self.saveState())
+        QSettings().setValue('mainwindow/geometry', self.saveGeometry())
+        QSettings().setValue('mainwindow/state', self.saveState())
 
     def load_settings(self):
-        geometry = QSettings().value("mainwindow/geometry")
+        geometry = QSettings().value('mainwindow/geometry')
         if isinstance(geometry, QByteArray):
             self.restoreGeometry(geometry)
-        
-        state = QSettings().value("mainwindow/state")
+
+        state = QSettings().value('mainwindow/state')
         if isinstance(state, QByteArray):
             self.restoreState(state)
 
