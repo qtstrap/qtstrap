0ad1a6ff18d10c252965e7576a3f337a58e8de8d
David Kincaid <daelonsuzuka@gmail.com>
2023-09-09 01:46:03 -0400

    Add type hints and fix linter warnings

--- qtstrap/widgets/layouts.py
+++ qtstrap/widgets/layouts.py
@@ -1,4 +1,20 @@
-from qtstrap import *
+from typing import TypeAlias, Literal
+from qtpy.QtCore import (
+    Qt,
+    QSettings,
+    QMargins,
+)
+from qtpy.QtWidgets import (
+    QMainWindow,
+    QWidget,
+    QLayout,
+    QSplitter,
+    QVBoxLayout,
+    QHBoxLayout,
+    QGridLayout,
+    QFormLayout,
+    QScrollArea,
+)
 
 
 alignments = {
@@ -15,6 +31,27 @@ alignments = {
     'c': Qt.AlignCenter,
 }
 
+AlignmentType: TypeAlias = (
+    Literal[
+        Qt.AlignLeft,
+        Qt.AlignRight,
+        Qt.AlignTop,
+        Qt.AlignBottom,
+        Qt.AlignCenter,
+        'left',
+        'l',
+        'right',
+        'r',
+        'top',
+        't',
+        'bottom',
+        'bot',
+        'b',
+        'center',
+        'c',
+    ]
+    | None
+)
 
 orientations = {
     'horizontal': Qt.Horizontal,
@@ -23,9 +60,45 @@ orientations = {
     'v': Qt.Vertical,
 }
 
+OrientationType: TypeAlias = (
+    Literal[
+        Qt.Horizontal,
+        Qt.Horizontal,
+        Qt.Vertical,
+        Qt.Vertical,
+        'horizontal',
+        'h',
+        'vertical',
+        'v',
+    ]
+    | None
+)
 
-class ContextLayout:
-    def __init__(self, parent=None, stretch=None, margins=None, align=None, **kwargs):
+MarginsType: TypeAlias = QMargins | tuple | int | None
+
+
+class ContextLayoutBase:
+    def add(
+        self, item: QWidget | QLayout | list[QWidget | QLayout], *args, **kwargs
+    ) -> QWidget | QLayout | list[QWidget | QLayout]:
+        return item
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        pass
+
+
+class ContextLayout(ContextLayoutBase):
+    def __init__(
+        self,
+        parent: QMainWindow | QSplitter | QWidget | ContextLayoutBase | None = None,
+        stretch: int | None = None,
+        margins: QMargins | tuple | int | None = None,
+        align: AlignmentType = None,
+        **kwargs,
+    ):
         if isinstance(parent, QMainWindow):
             # a layout can't be added directly to a QMainWindow, because it requires a "centralWidget"
             # we can use a throwaway QWidget as a shim
@@ -56,10 +129,10 @@ class ContextLayout:
         if align in alignments:
             self.setAlignment(alignments[align])
 
-        self._stack = []
-        self.next_layout = None
+        self._stack: list[ContextLayoutBase] = []
+        self.next_layout: ContextLayoutBase | None = None
 
-    def __getattr__(self, name):
+    def __getattr__(self, name: str):
         return getattr(self._layout, name)
 
     @property
@@ -72,11 +145,19 @@ class ContextLayout:
     def __call__(self):
         return self._layout
 
-    def __iadd__(self, item):
+    def __iadd__(
+        self,
+        item: QWidget | QLayout | list[QWidget | QLayout],
+    ):
         self.add(item)
         return self
 
-    def add(self, item, *args, **kwargs):
+    def add(
+        self,
+        item: QWidget | QLayout | list[QWidget | QLayout],
+        *args,
+        **kwargs,
+    ) -> QWidget | QLayout | list[QWidget | QLayout]:
         if isinstance(item, QWidget):
             self._layout.addWidget(item, *args, **kwargs)
         elif isinstance(item, QLayout):
@@ -103,14 +184,14 @@ class ContextLayout:
         self.next_layout = CFormLayout(self._layout, *args, **kwargs)
         return self
 
-    def split(self, name=None, **kwargs):
+    def split(self, name: str | None = None, **kwargs):
         if name:
             self.next_layout = PersistentCSplitter(name, self._layout, **kwargs)
         else:
             self.next_layout = CSplitter(self._layout, **kwargs)
         return self
 
-    def scroll(self, name=None, **kwargs):
+    def scroll(self, name: str | None = None, **kwargs):
         if name:
             self.next_layout = PersistentCScrollArea(name, self._layout, **kwargs)
         else:
@@ -186,17 +267,26 @@ class CFormLayout(ContextLayout, QFormLayout):
 # --------------------------------------------------------------------------- #
 
 
-class CSplitter(QSplitter):
-    def __init__(self, parent=None, margins=None, orientation=None, **kwargs):
+class CSplitter(QSplitter, ContextLayoutBase):
+    def __init__(
+        self,
+        parent: QWidget | QLayout | ContextLayoutBase | None = None,
+        margins: QMargins | tuple | int | None = None,
+        orientation: OrientationType | None = None,
+        **kwargs,
+    ):
         if orientation in orientations:
             orientation = orientations[orientation]
 
         if isinstance(parent, QWidget):
             super().__init__(parent, orientation, **kwargs)
             CHBoxLayout(parent, margins=margins).add(self)
-        elif isinstance(parent, QLayout):
+        elif isinstance(parent, ContextLayoutBase):
             super().__init__(orientation, **kwargs)
             parent.add(self)
+        elif isinstance(parent, QLayout):
+            # TODO: implement this
+            pass
 
         if margins is not None:
             if isinstance(margins, tuple):
@@ -207,11 +297,18 @@ class CSplitter(QSplitter):
         if orientation:
             self.setOrientation(orientation)
 
-    def __iadd__(self, item):
+    def __iadd__(
+        self,
+        item: QWidget | QLayout | list[QWidget | QLayout],
+    ):
         self.add(item)
         return self
 
-    def add(self, item, stretch=None):
+    def add(
+        self,
+        item: QWidget | QLayout | list[QWidget | QLayout],
+        stretch: int | None = None,
+    ) -> QWidget | QLayout | list[QWidget | QLayout]:
         if isinstance(item, QWidget):
             self.addWidget(item)
             if stretch:
@@ -232,10 +329,15 @@ class CSplitter(QSplitter):
         pass
 
 
-class PersistentCSplitter(CSplitter):
-    def __init__(self, name, parent=None, **kwargs):
+class PersistentCSplitter(CSplitter, ContextLayoutBase):
+    def __init__(
+        self,
+        name: str,
+        parent: QWidget | QLayout | ContextLayoutBase | None = None,
+        **kwargs,
+    ):
         super().__init__(parent, **kwargs)
-        self.name = name
+        self.name: str = name
 
         self.splitterMoved.connect(lambda: QSettings().setValue(self.name, self.saveState()))
 
@@ -250,14 +352,22 @@ class PersistentCSplitter(CSplitter):
 # --------------------------------------------------------------------------- #
 
 
-class CScrollArea(QScrollArea):
-    def __init__(self, parent=None, margins=None, orientation=None, **kwargs):
+class CScrollArea(QScrollArea, ContextLayoutBase):
+    def __init__(
+        self,
+        parent: QWidget | QLayout | ContextLayoutBase | None = None,
+        margins: QMargins | tuple | int | None = None,
+        orientation: OrientationType = None,
+        **kwargs,
+    ):
         super().__init__(**kwargs)
 
         if isinstance(parent, QWidget):
             CVBoxLayout(parent).add(self)
-        elif isinstance(parent, QLayout):
+        elif isinstance(parent, ContextLayoutBase):
             parent.add(self)
+        elif isinstance(parent, QLayout):
+            parent.addLayout(self)
 
         widget = QWidget()
         layout = CVBoxLayout(widget)
@@ -273,11 +383,18 @@ class CScrollArea(QScrollArea):
                 orientation = orientations[orientation]
             self.setOrientation(orientation)
 
-    def __iadd__(self, item):
+    def __iadd__(
+        self,
+        item: QWidget | QLayout | list[QWidget | QLayout],
+    ):
         self.add(item)
         return self
 
-    def add(self, item, stretch=None):
+    def add(
+        self,
+        item: QWidget | QLayout | list[QWidget | QLayout],
+        stretch: int = None,
+    ) -> QWidget | QLayout | list[QWidget | QLayout]:
         if isinstance(item, QWidget):
             self.widget().layout().addWidget(item)
             if stretch:
@@ -304,14 +421,19 @@ class CScrollArea(QScrollArea):
         pass
 
 
-class PersistentCScrollArea(QScrollArea):
-    def __init__(self, name, parent=None, **kwargs):
+class PersistentCScrollArea(QScrollArea, ContextLayoutBase):
+    def __init__(
+        self,
+        name: str,
+        parent: QWidget | QLayout | ContextLayoutBase | None = None,
+        **kwargs,
+    ):
         super().__init__(parent, **kwargs)
         self.name = name
 
         self.scrolled.connect(lambda: QSettings().setValue(self.name, self.saveState()))
 
-    def scroll_to(self, value):
+    def scroll_to(self, value: int):
         self.verticalScrollBar().setValue(value)
 
     def restore_state(self):
