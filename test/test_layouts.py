1746eebc22c3e2e843f8b2b52244414f99850b73
David Kincaid <daelonsuzuka@gmail.com>
2023-09-09 01:46:22 -0400

    Add more layout test cases

--- test/test_layouts.py
+++ test/test_layouts.py
@@ -1,20 +1,36 @@
 from qtstrap import (
+    CHBoxLayout,
     CVBoxLayout,
     CGridLayout,
-    get_children
+    get_children,
+)
+from qtpy.QtCore import (
+    QMargins,
 )
 from qtpy.QtWidgets import (
     QLabel,
     QWidget,
 )
 
+
 def test_context_layout():
+    def get_hbox():
+        with CHBoxLayout() as hbox:
+            hbox.add(QLabel('H1'))
+            hbox.add(QLabel('H2'))
+        return hbox
+
     widget = QWidget()
 
     with CVBoxLayout(widget) as layout:
         layout.add(QLabel('test'))
+        layout += QLabel('test2')
+        layout.add([QLabel('test3'), QLabel('test4')])
+        layout += [QLabel('test5'), QLabel('test6')]
+        layout.add(get_hbox())
+        layout += get_hbox()
 
-    assert len(get_children(widget)) == 2
+    assert len(get_children(widget)) == 13
 
 
 def test_nested_context_layout():
@@ -25,6 +41,32 @@ def test_nested_context_layout():
             layout.add(QLabel('upper left'))
             layout.add(QLabel('upper right'))
         with layout.hbox() as layout:
+            layout.add(QLabel('lower left'))
+            layout.add(QLabel('lower right'))
+
+    assert len(get_children(widget)) == 7
+
+
+def test_formlayout():
+    pass
+
+
+def test_splitter():
+    pass
+
+
+def test_scrollarea():
+    pass
+
+
+def test_margins_formats():
+    widget = QWidget()
+
+    with CVBoxLayout(widget, margins=0) as layout:
+        with layout.hbox(margins=(5, 5, 5, 5)) as layout:
+            layout.add(QLabel('upper left'))
+            layout.add(QLabel('upper right'))
+        with layout.hbox(margins=QMargins(10, 10, 10, 10)) as layout:
             layout.add(QLabel('lower left'))
             layout.add(QLabel('lower right'))
 
