c8c87aad75190bc41edfd00d468040eee5134141
Daelon Suzuka <daelonsuzuka@gmail.com>
2024-05-08 01:06:32 -0400

    Fix memory leak by removing expired timers

--- qtstrap/utils/call_later.py
+++ qtstrap/utils/call_later.py
@@ -1,4 +1,4 @@
-from qtstrap import *
+from qtpy.QtCore import QTimer
 
 
 _call_timers = []
@@ -8,5 +8,6 @@ def call_later(what, msec: int = 1):
     """call the given function after a specified delay"""
     timer = QTimer(singleShot=True)
     timer.timeout.connect(what)
+    timer.timeout.connect(lambda: _call_timers.remove(timer))
     timer.start(msec)
     _call_timers.append(timer)
