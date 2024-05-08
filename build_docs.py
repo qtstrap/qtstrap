0e61e722bbb6231c586f2a33d55b65b12172e479
Daelon Suzuka <daelonsuzuka@gmail.com>
2024-05-08 04:07:24 -0400

    Fix source links

--- build_docs.py
+++ build_docs.py
@@ -5,6 +5,7 @@ generate_docs(
     ["qtstrap"],
     output_path="./docs/api",
     overview_file='overview.md',
+    src_base_url='https://github.com/qtstrap/qtstrap/blob/master',
     ignored_modules = [
         'qt',
         'version',
